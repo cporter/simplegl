91766b261f76be916d2d5c703119930c96ee1955
Corey Porter <coreyaporter@gmail.com>
2013-03-15 14:59:07 -0700

    Adding support for glBufferData and friends

--- parse.py
+++ parse.py
@@ -7,7 +7,7 @@ allowed = set(['GLenum','GLbyte','GLshort','GLint','GLsizei', 'GLboolean',
                'GLdouble','GLclampd'])
 
 const_re = re.compile('#define\s+(GL_[A-Z0-9_]+)\s+(0x[0-9]+)')
-meth_re = re.compile('extern\s+([A-Za-z_0-9]+)\s+([A-Za-z0-9]+)\s+\(([^)]+)\)\s*;')
+meth_re = re.compile('\s*([A-Za-z_0-9]+)\s+(gl[A-Za-z0-9]+)\s*\(\s*([^)\[\]]+)\)\s*;')
 
 def argz(s):
     if 'void' == s:
@@ -33,14 +33,54 @@ def process_const(line):
 def is_ptr(x):
     return x.endswith('*')
 
-def process_meth(line, meth, getter):
+def is_v_meth(typ, nam, args):
+    return \
+        nam.endswith(typ) and \
+        all(x in allowed for x in args[:-1]) and \
+        args[-1] == 'GLfloat*'
+
+___bad = ['extern', 'GLAPIENTRY', 'GLAPI', 'APIENTRY', 'APIENTRYP']
+def fix_line (l):
+    for x in ___bad:
+        l = l.replace(x, '')
+    return l.strip()
+
+def process_meth(line, env):
+    line = fix_line (line)
+    meth = env.get_template('meth.tmpl')
+    getter = env.get_template('getter.tmpl')
+    floatv = env.get_template('floatv.tmpl')
     m = meth_re.search(line)
     if not m:
         return
     ret = m.group(1)
     nam = m.group(2)
     args = argz(m.group(3))
-    if 'glGet' == nam[:5] and 1 < len(args) and all(x in allowed for x in args[:-1]) and is_ptr(args[-1]):
+    if is_v_meth ('fv', nam, args):
+        print '// FV'
+        print (floatv.render(meth = { 'name' : nam,
+                                      'typ' : 'GLfloat',
+                                      'args' : args[:-1] }))
+        return True
+    elif is_v_meth ('iv', nam, args):
+        print '// IV'
+        print (floatv.render(meth = { 'name' : nam,
+                                      'typ' : 'GLint',
+                                      'args' : args[:-1] }))
+        return True
+    elif is_v_meth ('dv', nam, args):
+        print '// DV'
+        print (floatv.render(meth = { 'name' : nam,
+                                      'typ' : 'GLdouble',
+                                      'args' : args[:-1] }))
+        return True
+    elif is_v_meth ('sv', nam, args):
+        print '// SV'
+        print (floatv.render(meth = { 'name' : nam,
+                                      'typ' : 'GLshort',
+                                      'args' : args[:-1] }))
+        return True
+    elif 'glGet' == nam[:5] and 1 < len(args) and all(x in allowed for x in args[:-1]) and is_ptr(args[-1]):
         if 'GLvoid' != destar(args[-1]):
             sig = { 'name' : nam, 'args' : args[:-1], 'ret' : destar(args[-1]) }
             print(getter.render(meth = sig))
@@ -53,18 +93,14 @@ def process_meth(line, meth, getter):
             sig['return_type'] = ret
         print (meth.render(meth = sig))
         return True
-    else:
-        print >>sys.stderr, 'SKPPING %s' % line.strip ()
     return False
 
 def process(fp, env):
-    meth = env.get_template('meth.tmpl')
-    getter = env.get_template('getter.tmpl')
     for line in fp:
         count = 0
         if process_const(line):
             count = 1 + count
-        if process_meth(line, meth, getter):
+        if process_meth(line, env):
             count = 1 + count
         if 0 == count:
             pass
--- src/geeell_by_hand.cc
+++ src/geeell_by_hand.cc
@@ -2,10 +2,23 @@
 #include "geeell.h"
 #include "convert.h"
 
+#include <vector>
+#include <functional>
+
 using namespace convert;
 using namespace v8;
 
 #define GL_SIZEOF(NAM) size_of -> Set (String::NewSymbol(#NAM), Number::New (sizeof (NAM)))
+
+template <typename T>
+void WithVector (Handle<Array> arr, std::function<void(std::vector<T>&)> f) {
+  const size_t N = arr -> Length ();
+  std::vector<T> vect;
+  vect . reserve (N);
+  for (size_t i = 0; i < N; ++i)
+    vect . push_back (JSTO<T> (arr -> Get (i)));
+  f (vect);
+}
 
 void init_gl_byhand (Handle<Object> gl) {
   Handle<Object> size_of = Object::New ();
@@ -45,4 +58,111 @@ void init_gl_byhand (Handle<Object> gl) {
       });
     gl -> Set (String::NewSymbol ("glShaderSource"), tmpl -> GetFunction ());
   }
+
+  // glBufferData and glBufferSubData (also the ARB versions) we'll handle as a special case (for now)
+
+  { Local<FunctionTemplate> tmpl =
+      FunctionTemplate::New
+      (JS_FUNC {
+        if (4 > args . Length ())
+          return ThrowException
+            (String::NewSymbol("Not enough arguments for glBufferData (needs at least 4)"));
+
+        GLenum target = JSTO<GLenum> (args[0]);
+        GLsizeiptr size = JSTO<GLsizeiptr> (args[1]);
+        Handle<Array> arr = Handle<Array>::Cast (args[2]);
+        GLenum usage = JSTO<GLenum> (args[3]);
+
+        if (4 < args . Length () && "double" == JSTO<std::string> (args[4])) {
+          WithVector<GLdouble> (arr, [target, size, usage](std::vector<GLdouble> &v) {
+              glBufferData (target, size, &v[0], usage);
+            });
+        } else {
+          WithVector<GLfloat> (arr, [target, size, usage](std::vector<GLfloat> &v) {
+              glBufferData (target, size, &v[0], usage);
+            });
+        }
+        return Undefined ();
+      });
+    gl -> Set (String::NewSymbol ("glBufferData"), tmpl -> GetFunction ());
+  }
+
+  { Local<FunctionTemplate> tmpl =
+      FunctionTemplate::New
+      (JS_FUNC {
+        if (4 > args . Length ())
+          return ThrowException
+            (String::NewSymbol("Not enough arguments for glBufferData (needs at least 4)"));
+
+        GLenum target = JSTO<GLenum> (args[0]);
+        GLintptr offset = JSTO<GLintptr> (args[1]);
+        GLsizeiptr size = JSTO<GLsizeiptr> (args[2]);
+        Handle<Array> arr = Handle<Array>::Cast (args[3]);
+
+        if (4 < args . Length () && "double" == JSTO<std::string> (args[4])) {
+          WithVector<GLdouble> (arr, [target, offset, size](std::vector<GLdouble> &v) {
+              glBufferSubData (target, offset, size, &v[0]);
+            });
+        } else {
+          WithVector<GLfloat> (arr, [target, offset, size](std::vector<GLfloat> &v) {
+              glBufferSubData (target, offset, size, &v[0]);
+            });
+        }
+        return Undefined ();
+      });
+    gl -> Set (String::NewSymbol ("glBufferSubData"), tmpl -> GetFunction ());
+  }
+
+  { Local<FunctionTemplate> tmpl =
+      FunctionTemplate::New
+      (JS_FUNC {
+        if (4 > args . Length ())
+          return ThrowException
+            (String::NewSymbol("Not enough arguments for glBufferData (needs at least 4)"));
+
+        GLenum target = JSTO<GLenum> (args[0]);
+        GLsizeiptr size = JSTO<GLsizeiptr> (args[1]);
+        Handle<Array> arr = Handle<Array>::Cast (args[2]);
+        GLenum usage = JSTO<GLenum> (args[3]);
+
+        if (4 < args . Length () && "double" == JSTO<std::string> (args[4])) {
+          WithVector<GLdouble> (arr, [target, size, usage](std::vector<GLdouble> &v) {
+              glBufferDataARB (target, size, &v[0], usage);
+            });
+        } else {
+          WithVector<GLfloat> (arr, [target, size, usage](std::vector<GLfloat> &v) {
+              glBufferDataARB (target, size, &v[0], usage);
+            });
+        }
+        return Undefined ();
+      });
+    gl -> Set (String::NewSymbol ("glBufferDataARB"), tmpl -> GetFunction ());
+  }
+
+  { Local<FunctionTemplate> tmpl =
+      FunctionTemplate::New
+      (JS_FUNC {
+        if (4 > args . Length ())
+          return ThrowException
+            (String::NewSymbol("Not enough arguments for glBufferData (needs at least 4)"));
+
+        GLenum target = JSTO<GLenum> (args[0]);
+        GLintptr offset = JSTO<GLintptr> (args[1]);
+        GLsizeiptr size = JSTO<GLsizeiptr> (args[2]);
+        Handle<Array> arr = Handle<Array>::Cast (args[3]);
+
+        if (4 < args . Length () && "double" == JSTO<std::string> (args[4])) {
+          WithVector<GLdouble> (arr, [target, offset, size](std::vector<GLdouble> &v) {
+              glBufferSubDataARB (target, offset, size, &v[0]);
+            });
+        } else {
+          WithVector<GLfloat> (arr, [target, offset, size](std::vector<GLfloat> &v) {
+              glBufferSubDataARB (target, offset, size, &v[0]);
+            });
+        }
+        return Undefined ();
+      });
+    gl -> Set (String::NewSymbol ("glBufferDataARB"), tmpl -> GetFunction ());
+  }
+
 }
